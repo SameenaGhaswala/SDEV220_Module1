4838bf5900c1fc43a9df47d85a410b4a60370c1c
Sameena Ghaswala <ghaswalasameena0203@gmail.com>
2026-09-01 20:58:47 -0400

    Lab activity 1

---
+++ Lab_Activity1.py
@@ -0,0 +1,10 @@
+fic_character = "Moana"
+fic_age = 19
+fic_hero = True
+
+print(f"My favorite character is {fic_character}, and she is {fic_age} years old. \nIs she the hero of the movie? Absolutely {fic_hero}")
+
+fic_age += 5
+fic_hero = not fic_hero
+
+print(f"\nMy favorite character is {fic_character}, and she is {fic_age} years old. \nIs she the hero of the movie? Absolutely {fic_hero}")

feb03e71800ba2f5525bea37e8ea67fef0214537
Sameena Ghaswala <ghaswalasameena0203@gmail.com>
2026-09-06 13:19:51 -0400

    Chapter_4 practice questions

---
+++ Que_4.2.py
@@ -0,0 +1,8 @@
+s1 = "roast beef"
+s2 = "ham"
+s3 = "head"
+s4 = "clam"
+print(f"""My kitty cat likes {s1},
+My kitty cat likes {s2},
+My kitty cat fell on his {s3}
+And now thinks he's a {s4}.""")
---
+++ Que_4.7.py
@@ -0,0 +1,7 @@
+duck = "Ducky McDuckface"
+gourd = "Gourdy McGourdface"
+spitz = "Spitzy McSpitzface"
+
+print(f"The winning name for the duck prize is {duck}")
+print(f"The winning name for the gourd prize is {gourd}")
+print(f"The winning name for the spitz prize is {spitz}")
